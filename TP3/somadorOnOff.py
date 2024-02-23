71c40207c884b4c690163e9290a1fdbff6a34e3d
afonsoabc36 <silva.af36@gmail.com>
2024-02-23 15:26:08 +0000

    Brainstorm of TPC3

---
+++ TP3/somadorOnOff.py
@@ -0,0 +1,14 @@
+import sys
+import re
+
+def main():
+    # match e retirar as strings dentro do on e do off
+    # parse das strings, retirar os números e os iguais em função auxiliar
+    # passar como argumento um bool a dizer se está dentro de um on maybe
+    # se encontrar um número junta à soma, se encontrar um igual dá print à soma
+    # Maybe juntar tudo a uma lista e processar os números e prints por ordem no main
+    # repetir e iterar por todas
+    pass
+
+if __name__ == '__main__':
+    main()
