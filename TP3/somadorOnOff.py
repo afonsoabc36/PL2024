2a11d2277762480ef9a4af08c7aa7c53f16d34e5
afonsoabc36 <silva.af36@gmail.com>
2024-02-25 14:55:05 +0000

    Working for most cases, still working on specifics

--- TP3/somadorOnOff.py
+++ TP3/somadorOnOff.py
@@ -1,14 +1,54 @@
 import sys
 import re
 
+def processText(text, sum, openOn):
+    partialSum = 0
+    matches = re.findall(r'\d+|=',text) # Encontrar números e '='
+
+    for match in matches:
+        if match == "=":
+            print(sum + partialSum)
+        else:
+            if openOn:
+                partialSum += int(match)
+
+    sum += partialSum
+    return sum
+
 def main():
+    sum = 0
+    openOn = False
+
+    for line in sys.stdin:
+        if openOn:
+            offMatch = re.match(r'(.*)\s+off\s+', line)
+            if offMatch:
+                sum = processText(line, sum, openOn)
+                openOn = False
+            else:
+                sum = processText(line, sum, openOn)
+        else:
+            fullMatch = re.match(r'.*\s+on\s+(.*?)\s+off\s+.*$', line)
+            onMatch = re.match(r'.*\s+on\s+(.*)$',line)
+            if fullMatch:
+                openOn = True
+                sum = processText(fullMatch.group(1), sum, openOn)
+                openOn = False
+            elif onMatch:
+                openOn = True
+                sum = processText(onMatch.group(1), sum, openOn)
+            else: # elif
+                sum = processText(line, sum, openOn)
     # match e retirar as strings dentro do on e do off
+    # maybe findall mas pode não funcionar por ser linhas diferentes
+    # if not openOn : try findall começa on acaba off, se for none começa match on(.) e fazer o parse auxiliar de \1
+    # if openOn : try match (.*?)off
+    # verificar se off ou \s+off\s+
     # parse das strings, retirar os números e os iguais em função auxiliar
     # passar como argumento um bool a dizer se está dentro de um on maybe
     # se encontrar um número junta à soma, se encontrar um igual dá print à soma
     # Maybe juntar tudo a uma lista e processar os números e prints por ordem no main
     # repetir e iterar por todas
-    pass
 
 if __name__ == '__main__':
     main()
